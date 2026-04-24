b1096bc7f4bf667e17359a01c4bcfeb41686c70f
utsavpatel51 <utsavpatel51@gmail.com>
2026-04-24 16:57:16 +0530

    Adding vercel analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 export const viewport = {
 	width: "device-width",
@@ -44,7 +45,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<body>{children}</body>
+			<body>
+				{children}
+				<Analytics />
+			</body>
 		</html>
 	);
 }
